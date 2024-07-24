4b9c428b6f9b02663fd9cee108739723eed4bce6
Estivenson Ortega <estivenson.ortega@softwareone.com>
2024-07-24 07:12:48 -0500

    Creación del proyecto ComiteTecnicoMaui.App

---
+++ ComiteTecnicoMaui.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ComiteTecnicoMaui.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
